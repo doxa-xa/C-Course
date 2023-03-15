5c6b1add229979f5c02679debfc916159a7d43ab
doxa-xa <c.todopob@gmail.com>
2023-03-15 12:12:13 +0200

    OOP encapsulation

--- classes.cpp
+++ classes.cpp
@@ -3,26 +3,54 @@
 using namespace std;
 
 class YouTubeChannel{
-public:
     string Name;
     string OwnersName;
     int SubscribersCount;
     list<string> PublishedVideoTitles;
+public:
+    YouTubeChannel(string name, string ownersName){
+        Name = name;
+        OwnersName = ownersName;
+        SubscribersCount = 0;
+    }
+    void getInfo(){
+        cout<<"Name: "<<Name<<&endl;
+        cout<<"Owners Name: "<<OwnersName<<&endl;
+        cout<<"Subscribers: "<<SubscribersCount<<&endl;
+        cout<<"Videos: ";
+        for(string VideoTitle : PublishedVideoTitles){
+            cout<<VideoTitle<<" ";
+        }
+    }
+    void Subscribe(){
+        if(SubscribersCount>0)
+            SubscribersCount++;
+    }
+    void Unsubscribe(){
+        SubscribersCount--;
+    }
+    void PublishVideo(string title){
+        PublishedVideoTitles.push_back(title);
+    }
+    void SetOwnersName(string name){
+        OwnersName = name;
+    }
+    string GetOwnersName(){
+        return OwnersName;
+    }
+    void SetYTChannelName(string name){
+        Name = name;
+    }
+    string GetYTChannelName(){
+        return Name;
+    }
 };
 
 int main(){
-    YouTubeChannel ytChannel;
-    ytChannel.Name = "C++";
-    ytChannel.OwnersName = "Stoyan";
-    ytChannel.SubscribersCount = 1500;
-    ytChannel.PublishedVideoTitles = {"OOP with C++","Functions","Recursive functions","Classes"};
-
-    cout<<"Name: "<<ytChannel.Name<<&endl;
-    cout<<"Owners Name: "<<ytChannel.OwnersName<<&endl;
-    cout<<"Subscribers: "<<ytChannel.SubscribersCount<<&endl;
-    cout<<"Videos: ";
-    for(string VideoTitle : ytChannel.PublishedVideoTitles){
-        cout<<VideoTitle<<" ";
-    }
-    
+    YouTubeChannel ytChannel("C++","Stoyan");
+    ytChannel.PublishVideo("OOP with C++");
+    ytChannel.PublishVideo("Functions");
+    ytChannel.PublishVideo("Recursive functions");
+    ytChannel.PublishVideo("Classes");
+    ytChannel.getInfo();    
 }
