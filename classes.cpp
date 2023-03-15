4cd35ae2ad362bf005ce0bfdbbf3898a0621f904
doxa-xa <c.todopob@gmail.com>
2023-03-15 12:46:44 +0200

    classes polymorphism

--- classes.cpp
+++ classes.cpp
@@ -4,14 +4,17 @@ using namespace std;
 
 class YouTubeChannel{
     string Name;
-    string OwnersName;
     int SubscribersCount;
     list<string> PublishedVideoTitles;
+protected:
+    string OwnersName;
+    int ContentQuality;
 public:
     YouTubeChannel(string name, string ownersName){
         Name = name;
         OwnersName = ownersName;
         SubscribersCount = 0;
+        ContentQuality = 0;
     }
     void getInfo(){
         cout<<"Name: "<<Name<<&endl;
@@ -23,11 +26,11 @@ public:
         }
     }
     void Subscribe(){
-        if(SubscribersCount>0)
             SubscribersCount++;
     }
     void Unsubscribe(){
-        SubscribersCount--;
+        if(SubscribersCount>0)
+            SubscribersCount--;
     }
     void PublishVideo(string title){
         PublishedVideoTitles.push_back(title);
@@ -44,13 +47,67 @@ public:
     string GetYTChannelName(){
         return Name;
     }
+    void CheckAnalytics(){
+        if(ContentQuality<5){
+            cout<<Name<<" has bad quality content."<< &endl;
+        }
+        else
+            cout<<Name<<" has great quality content."<< &endl;
+    }
+};
+
+class CoockingYTChannel:public YouTubeChannel {
+public:
+    CoockingYTChannel(string name, string ownerName):YouTubeChannel(name, ownerName){
+
+    }
+    void Practice(){
+        string name = GetOwnersName();
+        cout<<name<<" is practicing coocking, learning new recepies, experimenting with spices..."<<&endl;
+        ContentQuality++;
+    }
+};
+class SingersYTChannel:public YouTubeChannel {
+public:
+    SingersYTChannel(string name, string ownerName):YouTubeChannel(name, ownerName){
+
+    }
+    void Practice(){
+        string name = GetOwnersName();
+        cout<<name<<" is taking singing classes, learning new songs, lerning how to dance..."<<&endl;
+        ContentQuality++;
+    }
 };
 
 int main(){
-    YouTubeChannel ytChannel("C++","Stoyan");
-    ytChannel.PublishVideo("OOP with C++");
-    ytChannel.PublishVideo("Functions");
-    ytChannel.PublishVideo("Recursive functions");
-    ytChannel.PublishVideo("Classes");
-    ytChannel.getInfo();    
+    // YouTubeChannel ytChannel("C++","Stoyan");
+    // ytChannel.PublishVideo("OOP with C++");
+    // ytChannel.PublishVideo("Functions");
+    // ytChannel.PublishVideo("Recursive functions");
+    // ytChannel.PublishVideo("Classes");
+    // ytChannel.getInfo();    
+
+    CoockingYTChannel ytChannel("Katy Pasteries","Sibila");
+    // ytChannel.PublishVideo("Chocolate Cake");
+    // ytChannel.PublishVideo("Cheese Cake");
+    // ytChannel.Subscribe();
+    // ytChannel.Subscribe();
+    // ytChannel.Subscribe();
+    // ytChannel.Subscribe();
+    // ytChannel.getInfo();
+    ytChannel.Practice();
+
+    SingersYTChannel singerYTChannel("John Sings","John");
+    singerYTChannel.Practice();
+    singerYTChannel.Practice();
+    singerYTChannel.Practice();
+    singerYTChannel.Practice();
+    singerYTChannel.Practice();
+
+    YouTubeChannel* yt1 = &ytChannel;
+    YouTubeChannel* yt2 = &singerYTChannel;
+
+    yt1->CheckAnalytics();
+    yt2->CheckAnalytics();
+
 }
